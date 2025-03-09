6eedd09ba73449d0e085a1b2495dbb5973e2adb3
Shivangi889 <90382744+iluckyvirani@users.noreply.github.com>
2025-03-09 20:11:50 +0530

    updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", eventRoutes);
 
-// module.exports = app; 
+module.exports = app; 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
